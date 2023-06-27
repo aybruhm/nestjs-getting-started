d7dc0c4ca9e3ab3b5ba87bd34691bb67b26f2981
Abram <israelvictory87@gmail.com>
2023-06-27 13:53:25 +0100

    Update: modified getTasks controller to define filters

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -5,19 +5,27 @@ import {
   Patch,
   Delete,
   Body,
+  Query,
   Param,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TasksFilterDto } from './dto/filter-tasks.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasks_service: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasks_service.getAllTasks();
+  getTasks(@Query() params: TasksFilterDto): Task[] {
+    // if filters are defined, call service to execute get tasks with filters method
+    // otherwise, just call service to execute get all tasks method
+    if (Object.keys(params).length) {
+      return this.tasks_service.getTasksWithFilters(params);
+    } else {
+      return this.tasks_service.getAllTasks();
+    }
   }
 
   @Get(':id')
