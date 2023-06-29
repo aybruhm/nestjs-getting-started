72a0085feeda097da8babb8c190ed32cc920f7e6
Abram <israelvictory87@gmail.com>
2023-06-29 15:08:11 +0100

    Update: set configurations in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,29 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task_management_db',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config_service: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config_service.get('DATABASE_HOST'),
+          port: parseInt(config_service.get('DATABASE_PORT')) || 3050,
+          username: config_service.get('DATABASE_USERNAME'),
+          password: config_service.get('DATABASE_PASSWORD'),
+          database: config_service.get('DATABASE_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
     }),
     AuthModule,
   ],
