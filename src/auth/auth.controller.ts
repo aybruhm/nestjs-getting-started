c89201d7f301593056ef3017bcd1eb9470b9b954
Abram <israelvictory87@gmail.com>
2023-06-29 11:04:16 +0100

    Update: implemented signup and signin controller handlers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private auth_service: AuthService) {}
+
+  @Post('signup')
+  async createNewUser(@Body() payload: AuthCredentialsDto): Promise<object> {
+    const user = await this.auth_service.signUp(payload);
+    return user;
+  }
+
+  @Post('signin')
+  async loginUser(@Body() payload: AuthCredentialsDto): Promise<object> {
+    const success = await this.auth_service.signIn(payload);
+    return success;
+  }
+}
