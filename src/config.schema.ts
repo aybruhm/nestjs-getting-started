c25a2520fc5427607ef10b3dbc27481b3601313d
Abram <israelvictory87@gmail.com>
2023-06-29 21:34:14 +0100

    Update: added APP_PORT to env

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,6 +1,7 @@
 import Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
+  APP_PORT: Joi.number().default(3000).required(),
   DATABASE_HOST: Joi.string().required(),
   DATABASE_PORT: Joi.number().default(5432).required(),
   DATABASE_USERNAME: Joi.string().required(),
--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const logger = new Logger();
+  const port = process.env.APP_PORT;
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  await app.listen(3000);
+  await app.listen(port);
   logger.log(`Application listening on port 3000`);
 }
 bootstrap();
